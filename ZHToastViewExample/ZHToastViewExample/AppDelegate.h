a6fcad9c587cca580560d139523b56dd31491ee9
pengzhihua <pengzhihua@bianxianmao.com>
2021-03-26 16:47:00 +0800

    feat:init project

---
+++ ZHToastViewExample/ZHToastViewExample/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ZHToastViewExample
+//
+//  Created by pzh on 2021/3/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ZHToastViewExample/ZHToastViewExample/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  ZHToastViewExample
+//
+//  Created by pzh on 2021/3/26.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    NSLog(@"This is a ZHToastView example!");
+}
+
+
+@end
